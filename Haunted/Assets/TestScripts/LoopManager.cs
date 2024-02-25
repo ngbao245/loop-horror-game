292667e46f0005082b08c80a013f77bbb97dfd1f
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-02-25 09:48:35 +0700

    test loop mechanism

---
+++ Haunted/Assets/TestScripts/LoopManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoopManager : MonoBehaviour
+{
+    public string nextSceneName = "Scene2 2"; // Specify the next scene to load
+
+    private bool isTransitioning = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+            SceneManager.LoadScene(nextSceneName);
+
+    }
+
+    private IEnumerator LoadNextSceneAsync()
+    {
+        // Load the next scene asynchronously
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextSceneName);
+
+        // Wait until the next scene is fully loaded
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Reset the flag after the scene is loaded
+        isTransitioning = false;
+    }
+}
