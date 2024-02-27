93e7f25c9e0b0efe210e99d866347613b09aa111
BaoBiBo <ng.hoangbao03@gmail.com>
2024-02-27 08:20:45 +0700

    Add flicker, add loop, merge code

---
+++ Loop/Assets/Scripts/Light/LightFlicker.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LightFlicker : MonoBehaviour
+{
+    public GameObject intText, light;
+    public bool toggle = true, interactable;
+    public Renderer lightBulb;
+    public Material offlight, onlight;
+    public AudioSource lightSwitchSound;
+
+    public Light myLight;
+    float defaultIntensity;
+    bool isOn;
+    float timer;
+    float delay;
+    public float maxInterval = 1;
+    public float maxFlicker = 0.2f;
+    public float maxOffDelay = 2f;
+
+
+    void Start()
+    {
+        defaultIntensity = myLight.intensity;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intText.SetActive(true);
+            interactable = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intText.SetActive(false);
+            interactable = false;
+        }
+    }
+    void Update()
+    {
+        if (interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                toggle = !toggle;
+                intText.SetActive(false);
+                interactable = false;
+            }
+        }
+        if (toggle == false)
+        {
+            light.SetActive(false);
+            lightBulb.material = offlight;
+        }
+        if (toggle == true)
+        {
+            light.SetActive(true);
+            lightBulb.material = onlight;
+
+            timer += Time.deltaTime;
+            if (timer > delay)
+            {
+                ToggleLight();
+            }
+        }
+    }
+
+    void ToggleLight()
+    {
+        isOn = !isOn;
+
+        if (isOn)
+        {
+            myLight.intensity = defaultIntensity;
+            delay = Random.Range(0, maxInterval);
+        }
+        else
+        {
+            myLight.intensity = Random.Range(0f, defaultIntensity);
+            delay = Random.Range(0, maxFlicker);
+
+            delay += maxOffDelay;
+        }
+
+        timer = 0;
+    }
+}
---
+++ Loop/Assets/Scripts/OpenDrawer.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class LookAndInteract : MonoBehaviour
+{
+    public GameObject drawer; // Reference to the drawer GameObject
+    public GameObject intText; // Reference to the interaction text
+    public AudioSource openSound; // Sound played when opening the drawer
+    public AudioSource closeSound; // Sound played when closing the drawer
+
+    private bool isOpen = false; // Flag to track if the drawer is open or closed
+
+    void Update()
+    {
+        // Check if the player is looking at the drawer and presses the "E" key
+        if (Input.GetKeyDown(KeyCode.E) && IsLookingAtDrawer())
+        {
+            if (isOpen)
+            {
+                CloseDrawer();
+            }
+            else
+            {
+                OpenDrawer();
+            }
+        }
+    }
+
+    // Function to check if the player is looking at the drawer
+    bool IsLookingAtDrawer()
+    {
+        // Raycast from the center of the screen
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        // If the ray hits the drawer collider and it is tagged as "Drawer", return true
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == drawer && drawer.CompareTag("Drawer"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Function to open the drawer
+    void OpenDrawer()
+    {
+        // Play open sound
+        openSound.Play();
+
+        // Perform any animation or interaction logic here
+
+        // Set isOpen flag to true
+        isOpen = true;
+
+        // Display interaction text
+        intText.SetActive(false);
+    }
+
+    // Function to close the drawer
+    void CloseDrawer()
+    {
+        // Play close sound
+        closeSound.Play();
+
+        // Perform any animation or interaction logic here
+
+        // Set isOpen flag to false
+        isOpen = false;
+
+        // Hide interaction text
+        intText.SetActive(false);
+    }
+}
