3b910f52302c30cce8e1394b2636bd9756c62112
BaoBiBo <ng.hoangbao03@gmail.com>
2024-03-02 15:40:34 +0700

    add scene 3 and 4, update scene 1 and 2

---
+++ Loop/Assets/Assets/danthaigames/DS 80s Television/Scripts/RotatingObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour {
+
+	public float rotationSpeed = 0;
+
+	void Update () {
+		transform.Rotate (0, Time.deltaTime * rotationSpeed, 0);	
+	}
+}
